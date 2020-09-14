7dc9b2e0f9671acc7fbb7580b3075940a805f350
volko <volkovwladislav@mail.ru>
2020-09-14 10:01:12 +0400

    2.2. Модель данных - Post. [#344606]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,12 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    int id;
+    LocalDateTime creationDate;
+    LocalDateTime updatingDate;
+    String href;
+    String name;
+    String description;
+}
