deb836e1af09f865b969e9e7278378a47dd129e7
volko <volkovwladislav@mail.ru>
2020-09-14 09:56:05 +0400

    2.1. Преобразование даты [#344609]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,14 +5,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= 5; i++) {
+            String2Date str2d = new String2Date();
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td: row) {
@@ -20,37 +17,8 @@ public class SqlRuParse {
                 System.out.println(href.attr("href"));
                 System.out.println(href.text());
                 Element time = td.parent().child(5);
-                System.out.println(stringToDate(time.text().toString()));
+                System.out.println(str2d.stringToDate(time.text()));
             }
         }
-    }
-
-    public static Timestamp stringToDate(String s) {
-        DateTimeFormatter form = DateTimeFormatter.ofPattern("d MM yy, HH:mm");
-        return Timestamp.valueOf(LocalDateTime.parse(changeToFormat(s), form));
-    }
-
-    public static String changeToFormat(String s) {
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("d MM yy");
-        if (s.contains("сегодня")) {
-            s = s.replaceAll("сегодня", f.format(LocalDate.now()));
-            return s;
-        } else if (s.contains("вчера")) {
-            s = s.replaceAll("вчера", f.format(LocalDate.now().minusDays(1)));
-            return s;
-        }
-        s = s.replaceAll("янв", "01");
-        s = s.replaceAll("фев", "02");
-        s = s.replaceAll("мар", "03");
-        s = s.replaceAll("апр", "04");
-        s = s.replaceAll("май", "05");
-        s = s.replaceAll("июн", "06");
-        s = s.replaceAll("июл", "07");
-        s = s.replaceAll("авг", "08");
-        s = s.replaceAll("сен", "09");
-        s = s.replaceAll("окт", "10");
-        s = s.replaceAll("ноя", "11");
-        s = s.replaceAll("дек", "12");
-        return s;
     }
 }
---
+++ src/main/java/ru/job4j/html/String2Date.java
@@ -0,0 +1,38 @@
+package ru.job4j.html;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class String2Date {
+
+    public Timestamp stringToDate(String s) {
+        DateTimeFormatter form = DateTimeFormatter.ofPattern("d MM yy, HH:mm");
+        return Timestamp.valueOf(LocalDateTime.parse(changeToFormat(s), form));
+    }
+
+    public String changeToFormat(String s) {
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("d MM yy");
+        if (s.contains("сегодня")) {
+            s = s.replaceAll("сегодня", f.format(LocalDate.now()));
+            return s;
+        } else if (s.contains("вчера")) {
+            s = s.replaceAll("вчера", f.format(LocalDate.now().minusDays(1)));
+            return s;
+        }
+        s = s.replaceAll("янв", "01");
+        s = s.replaceAll("фев", "02");
+        s = s.replaceAll("мар", "03");
+        s = s.replaceAll("апр", "04");
+        s = s.replaceAll("май", "05");
+        s = s.replaceAll("июн", "06");
+        s = s.replaceAll("июл", "07");
+        s = s.replaceAll("авг", "08");
+        s = s.replaceAll("сен", "09");
+        s = s.replaceAll("окт", "10");
+        s = s.replaceAll("ноя", "11");
+        s = s.replaceAll("дек", "12");
+        return s;
+    }
+}
