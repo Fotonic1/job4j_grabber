1d22e1bb11264ef0d1bc700b4a69a5cf7669dc43
volko <volkovwladislav@mail.ru>
2020-09-14 10:18:09 +0400

    2.3. Загрузка деталей поста. [#344607]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,9 +7,9 @@ import org.jsoup.select.Elements;
 
 
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
+    public static void list() throws Exception {
+        String2Date str2d = new String2Date();
         for (int i = 1; i <= 5; i++) {
-            String2Date str2d = new String2Date();
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td: row) {
@@ -20,5 +20,15 @@ public class SqlRuParse {
                 System.out.println(str2d.stringToDate(time.text()));
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String2Date str2d = new String2Date();
+        String href = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
+        Document doc = Jsoup.connect(href).get();
+        Elements row = doc.select(".msgBody");
+        System.out.println(row.next().first().text());
+        row = doc.select(".msgFooter");
+        System.out.println(str2d.stringToDate(row.first().text().split(" \\[")[0]));
     }
 }
